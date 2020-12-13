59ed3b98becf9bfcbca4139029797efc181505b8
MrFengHa <1109179567@qq.com>
2020-12-13 23:34:46 +0800

    Initial commit

---
+++ element/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <a href="#/button">点我显示button</a>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
